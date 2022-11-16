beba45461c0ee9e705bf30c872433da37230399e
Alex Kizilov <as_k@list.ru>
2022-11-16 12:25:32 +0400

    Finished Home Work

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using System.Security;
+
+Console.Clear();
+
+int[] GetUserNumbers(int[] numbers)
+{ 
+    string stop = string.Empty;
+    int i = 0;
+    while (stop != "stop")
+    {
+        stop =  Convert.ToString(Console.ReadLine());
+        if (stop != "stop")
+        {
+            numbers[i] = Convert.ToInt32(stop);
+            i++;
+        }
+    }
+    return numbers;
+}
+
+int GetNumberOfPositiv(int[] array)
+{
+    int count = 0;  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите числа");
+Console.WriteLine("Когда захотите остановиться, наберите - stop ");
+
+int[] numUser = new int[25];
+GetUserNumbers(numUser);
+Console.WriteLine();
+
+int numberOfPositiv = GetNumberOfPositiv(numUser);
+
+Console.WriteLine($"Из введенных чисел, количество больше 0 составляет: {numberOfPositiv}");
+Console.WriteLine();
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+
+double[] GetCross(double a1, double c1, double a2, double c2)
+{
+    double[] coord = new double [2];
+    coord[0] = Math.Round(((c2 - c1) / (a1 - a2)),2);
+    coord[1] = Math.Round((a1 * coord[0] + c1),2);
+    return coord;
+}
+
+Console.WriteLine("Уравнения вида: y = k * x + b");
+Console.WriteLine();
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+double[] cross = GetCross(k1, b1, k2, b2);
+Console.WriteLine($"Точка перечечения двух прямых: y = {k1} * x + {b1}   и   y = {k2} * x + {b2}");
+Console.WriteLine();
+Console.WriteLine($"Находится в координатах ({cross[0]}; {cross[1]})");
+Console.WriteLine();
